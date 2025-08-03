670f6237f93017d949b7add17e8d15c3adf56b80
Vilizar Denichin <denichinv@gmail.com>
2025-08-03 17:37:06 +0100

    chore: update app icon and rename project to DevQuiz

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -126,7 +126,7 @@ function App() {
         </div>
       ) : (
         <>
-          <h1>Quiz App</h1>
+          <h1>DevQuiz</h1>
           <p>
             Score: {score} / {questions.length}
           </p>
